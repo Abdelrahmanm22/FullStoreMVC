1f0d56be48d53ee69c18c712c5773bcba36549d8
Abdelrahmanm22 <abdra1396@gmail.com>
2025-01-24 01:20:34 +0200

    Add Entity Framework Packages that allows developers to work with databases using C# objects rather than writing raw SQL queries. (EF.Tools => for performing migrations, managing schema changes, scaffolding, and simplifying database-related tasks.)

--- Store/obj/Debug/net7.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net7.0/Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc306304313aa67c665ca7d19e753314bf646106")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
