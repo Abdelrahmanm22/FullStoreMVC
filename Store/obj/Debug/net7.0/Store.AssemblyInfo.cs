20dc95f43cb5f35626ef01e78a6804b6f7304ab4
Abdelrahmanm22 <abdra1396@gmail.com>
2025-01-25 06:58:23 +0200

    Add Admin Dropdown in the Navbar && Change the website name and icon

--- Store/obj/Debug/net7.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net7.0/Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6efa5c72d60371bbbe71ddd62bb9887f16bc7f86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f09fc51f1d80aef120801f8fb068ad38af6eacb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
