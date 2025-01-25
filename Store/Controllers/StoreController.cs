c8667f3dd675660015a895ed14d08bc3f5c670ef
Abdelrahmanm22 <abdra1396@gmail.com>
2025-01-25 07:37:49 +0200

    StoreController - Read Products and StoreController - Add Pagination

---
+++ Store/Controllers/StoreController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Contexts;
+using Store.Models;
+
+namespace Store.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly AppDbContext dbContext;
+        private readonly int pageSize = 8;
+
+        public StoreController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index(int pageIndex)
+        {
+            IQueryable<Product> query = dbContext.Products;
+            query = query.OrderByDescending(p => p.Id);
+
+            ///pagination functionality
+            if (pageIndex < 1) pageIndex = 1;
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / pageSize);
+            query = query.Skip((pageIndex-1)*pageSize).Take(pageSize);
+
+
+            var Products = query.ToList();
+            
+            ViewBag.Products = Products;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.PageIndex = pageIndex;
+            return View();
+        }
+    }
+}
--- Store/obj/Debug/net7.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net7.0/Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6faa862f355a34728fab63ee6f738d20e740072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed6394c30ebc9a0525313e99cd7bf16863995bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
